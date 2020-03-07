f6009d734900ee326c42f1dd27dc55d7873a8104
Генади Михайлов <genadimihaylov@gmail.com>
2020-03-07 17:21:47 +0200

    Renamed and added identity

---
+++ DanubeJourney/DanubeJourney/Data/DanubeJourneyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DanubeJourney.Data
+{
+    public class DanubeJourneyDbContext : IdentityDbContext
+    {
+        public DanubeJourneyDbContext(DbContextOptions<DanubeJourneyDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
